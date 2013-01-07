f4ee4973e517b859f5cb35c9b50445351ebf7c57
Konrad Gądek <kgadek@gmail.com>
2013-01-07 05:42:38 +0100

    Samousuwanie się wpisów w ddict

--- apps/amber_client/src/amber_client.erl
+++ apps/amber_client/src/amber_client.erl
@@ -16,11 +16,11 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 -export([start_link/0]).
 % api
--export([register_receiver/2, get_synnum/0, send_to_amber/2]).
+-export([register_receiver/2, deregister_receiver/1, get_synnum/0, send_to_amber/2]).
 
 % ROBOCLAW
 -define(ROBOCLAW_TI, {2,0}).
--export([motors_command/1, motors_command/2, motors_command/4, motors_demo1/0]).
+-export([motors_command/2, motors_command/4, motors_demo1/0]).
 % STARGAZER
 -define(STARGAZER_TI, {3,0}).
 -export([stargazer_order_position/0, stargazer_order_position/1,
@@ -28,8 +28,11 @@
          stargazer_subscribe_position/0, stargazer_subscribe_position/1]).
 
 -record(state, {aip, aport, socket, dict, synnumnext}).
--record(dispd_key, {dev_t :: non_neg_integer(), dev_i :: non_neg_integer(), synnum :: non_neg_integer()}).
--record(dispd_val, {recpid = self()}).
+-record(dispd_key, {dev_t  :: non_neg_integer(),
+        						dev_i  :: non_neg_integer(),
+        						synnum :: non_neg_integer()}).
+-record(dispd_val, {recpid = self() :: pid(),
+        						post            :: mfa() | fun(() -> any())}).
 
 start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, {?AMBERIP, ?AMBERPORT}, []).
 
@@ -43,26 +46,38 @@ init({AmberIP, AmberPort}) ->
 terminate(_Reason, #state{socket = Socket}) -> gen_udp:close(Socket).
 
 handle_info({udp, Socket, ?AMBERIP, ?AMBERPORT, FullMsg}, #state{socket=Socket, dict=Dict} = State) ->
-	{Hdr, MsgB} = router:unpack_msg(FullMsg),
-	#driverhdr{devicetype=DevT, deviceid=DevI} = Hdr,
-	Msg = drivermsg_pb:decode_drivermsg(MsgB),
-	Key = #dispd_key{dev_t=DevT,dev_i=DevI,synnum=Msg#drivermsg.acknum},
-	{value, #dispd_val{recpid=RecPid}} = gb_trees:lookup(Key, Dict),
-	case process_info(RecPid) of
-		undefined ->
-			NDict = gb_trees:delete_any(Key, Dict),
-			{noreply, State#state{dict=NDict}};
-		_ ->
-			RecPid ! {amber_client_msg, Hdr, Msg},
-			{noreply, State}
+	{#driverhdr{devicetype=DevT, deviceid=DevI} = Hdr, MsgB} = router:unpack_msg(FullMsg),
+	#drivermsg{acknum = AckNum} = Msg                        = drivermsg_pb:decode_drivermsg(MsgB),
+	Key = #dispd_key{dev_t=DevT, dev_i=DevI, synnum=AckNum},
+	case gb_trees:lookup(Key, Dict) of
+		{value, #dispd_val{recpid=RecPid, post=Post}} ->
+			case process_info(RecPid) of
+				undefined ->
+					NDict = gb_trees:delete_any(Key, Dict),
+					{noreply, State#state{dict=NDict}};
+				_ ->
+					RecPid ! {amber_client_msg, Hdr, Msg},
+					case Post of
+						F when is_function(F,0) -> F();
+						{F,A}                   -> apply(F,A);
+						{M,F,A}                 -> apply(M,F,A);
+						_ -> ok
+					end,
+					{noreply, State}
+			end;
+		_ -> {noreply, State}
 	end.
 
 handle_cast({send_to_amber, MsgB}, #state{aip=AIP, aport=APort, socket=Socket} = State) ->
 	ok = gen_udp:send(Socket, AIP, APort, MsgB),
 	{noreply, State}.
 
-handle_call({register_receiver, Key, Pid}, _From, #state{dict=Dict} = State) ->
-	NDict = gb_trees:enter(Key, Pid, Dict), 
+handle_call({register_receiver, Key, Value}, _From, #state{dict=Dict} = State) ->
+	NDict = gb_trees:enter(Key, Value, Dict), 
+	{reply, ok, State#state{dict=NDict}};
+
+handle_call({deregister_receiver, Key}, _From, #state{dict=Dict} = State) ->
+	NDict = gb_trees:delete_any(Key, Dict), 
 	{reply, ok, State#state{dict=NDict}};
 
 handle_call(get_synnum, _From, #state{synnumnext=SN} = State) ->
@@ -70,14 +85,23 @@ handle_call(get_synnum, _From, #state{synnumnext=SN} = State) ->
 
 code_change(_OldV, State, _Extra) -> {ok, State}.
 
+noop() -> ok.
+send_to_amber(MsgB) -> gen_server:cast(?MODULE, {send_to_amber, MsgB}).
 
-%% API -------------------------------------------------------------------------
 
--spec register_receiver(#dispd_key{}, pid())
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%[ API ]%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+-spec register_receiver(#dispd_key{}, #dispd_val{})
 			-> 'ok'.
-register_receiver(Key = #dispd_key{}, Pid) -> gen_server:call(?MODULE, {register_receiver, Key, Pid}).
+register_receiver(Key = #dispd_key{}, Val) ->
+	gen_server:call(?MODULE, {register_receiver, Key, Val}).
 
-% deregister_receiver({_DevT, _DevI, _SynNum}) -> 'ok'.
+-spec deregister_receiver(#dispd_key{})
+			-> 'ok'.
+deregister_receiver(Key = #dispd_key{}) ->
+	gen_server:call(?MODULE, {deregister_receiver, Key}).
 
 -spec get_synnum()
 			-> non_neg_integer().
@@ -87,9 +111,6 @@ get_synnum() -> gen_server:call(?MODULE, get_synnum).
 			-> 'ok'.
 send_to_amber(MsgH, MsgBinary) -> send_to_amber(router:pack_msg(MsgH, MsgBinary)).
 
-send_to_amber(MsgB) -> gen_server:cast(?MODULE, {send_to_amber, MsgB}).
-
-
 
 
 
@@ -97,65 +118,6 @@ send_to_amber(MsgB) -> gen_server:cast(?MODULE, {send_to_amber, MsgB}).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%[ ROBOCLAW ]%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-
-%% -----------------------------------------------------------------------------
-%% @doc Podstawowa procedura do poruszania robotem.
-%%
-%% Funkcja nieblokująca. Wysyła żądanie poruszania się kół robota.
-%% Jednostką prędkości jest mm/s.
-%%
-%% Teoretycznie 12000 pulsów/s to maksimum możliwości enkodera i silników.
-%% Empirycznie stwierdzona prędkość maksymalna to około 2200 mm/s.
-%%
-%% Funkcja może zwrócić błąd jedynie w wypadku błędu protobuf, np. gdy podamy
-%% string zamiast int.
-%%
-%% @deprecated Ze względu na format wejścia jest "overly-verbose" oraz
-%% stosunkowo powolna. Zalecam użycie {@link motors_command/4}.
-%% @end
-%% -----------------------------------------------------------------------------
--type motors_command_keys_int32()  :: 'front_m1_speed'    | 'front_m2_speed'
-																		| 'rear_m1_speed'     | 'rear_m2_speed'.
--type motors_command_keys_uint32() :: 'front_m1_accel'    | 'front_m1_distance'
-																		| 'front_m2_accel'    | 'front_m2_distance'
-																		| 'rear_m1_accel'     | 'rear_m1_distance'
-																		| 'rear_m2_accel'     | 'rear_m2_distance'.
--type motors_command_keys_bool()   :: 'front_m1_buffered' | 'front_m2_buffered'
-																		| 'rear_m1_buffered'  | 'rear_m2_buffered'.
--spec motors_command([ {motors_command_keys_int32(),  int32()}
-										 | {motors_command_keys_uint32(), uint32()}
-										 | {motors_command_keys_bool(),   boolean()} ])
-			-> 'ok'.
-motors_command(Os) ->
-	Rear = #motorscommand{
-		address = 16#80,
-		m1speed    = proplists:get_value(rear_right_speed,    Os),
-  	m1accel    = proplists:get_value(rear_right_accel,    Os),
-  	m1distance = proplists:get_value(rear_right_distance, Os),
-  	m1buffered = proplists:get_value(rear_right_buffered, Os),
-  	m2speed    = proplists:get_value(rear_left_speed,    Os),
-		m2accel    = proplists:get_value(rear_left_accel,    Os),
-		m2distance = proplists:get_value(rear_left_distance, Os),
-		m2buffered = proplists:get_value(rear_left_buffered, Os)
-  },
-  Front = #motorscommand{
-  	address = 16#81,
-		m1speed    = proplists:get_value(front_right_speed,     Os),
-  	m1accel    = proplists:get_value(front_right_accel,     Os),
-  	m1distance = proplists:get_value(front_right_distance,  Os),
-  	m1buffered = proplists:get_value(front_right_buffered,  Os),
-  	m2speed    = proplists:get_value(front_left_speed,     Os),
-		m2accel    = proplists:get_value(front_left_accel,     Os),
-		m2distance = proplists:get_value(front_left_distance,  Os),
-		m2buffered = proplists:get_value(front_left_buffered,  Os)
-  },
-	MsgBase = #drivermsg{type = 'DATA', synnum = get_synnum()},
-	{ok, Msg} = roboclaw_pb:set_extension(MsgBase, motorscommands, [Rear, Front]),
-	MsgBinary = roboclaw_pb:encode_drivermsg(Msg),
-	{DevT,DevI} = ?ROBOCLAW_TI,
-	Hdr = #driverhdr{devicetype = DevT, deviceid = DevI},
-	send_to_amber(Hdr, MsgBinary).
 
 
 %% @doc Uproszczone sterowanie robotem. Pozwala dobrać prędkość dla lewych i
@@ -192,19 +154,14 @@ motors_command(FL, FR, RL, RR) ->
 	send_to_amber(Hdr, MsgBinary).
 
 
+%% @hidden
 build_drivermsg(#motor_cmd{speed=L_Speed, accel=L_Accel, dist=L_Dist, is_buf=L_Buf},
                 #motor_cmd{speed=R_Speed, accel=R_Accel, dist=R_Dist, is_buf=R_Buf},
                 HwAddr) ->
 	#motorscommand{
-		address    = HwAddr,
-		m1speed    = R_Speed,
-		m1accel    = R_Accel,
-		m1distance = R_Dist,
-		m1buffered = R_Buf,
-		m2speed    = L_Speed,
-		m2accel    = L_Accel,
-		m2distance = L_Dist,
-		m2buffered = L_Buf
+		address = HwAddr,
+		m1speed = R_Speed, m1accel = R_Accel, m1distance = R_Dist, m1buffered = R_Buf,
+		m2speed = L_Speed, m2accel = L_Accel, m2distance = L_Dist, m2buffered = L_Buf
 	}.
 
 
@@ -244,28 +201,31 @@ stargazer_order_position(Os) ->
 	DevT = proplists:get_value(device_type, Os, DefDevT),
 	DevI = proplists:get_value(device_id, Os, DefDevI), 
 	Hdr = #driverhdr{devicetype = DevT, deviceid = DevI},
-	OrderedBy = proplists:get_value(pid, Os, self()), 
-	register_receiver({DevT, DevI, SynNum}, OrderedBy),
+	Key = #dispd_key{dev_t=DevT, dev_i=DevI, synnum=SynNum},
+	Val = #dispd_val{recpid = proplists:get_value(pid, Os, self()),
+									 post = {fun deregister_receiver/1, Key}},
+	register_receiver(Key, Val),
 	send_to_amber(Hdr, MsgBinary),
 	SynNum.
 
 stargazer_subscribe_position() -> stargazer_subscribe_position([]).
+
 stargazer_subscribe_position(Os) ->
-	% SynNum = get_synnum(),
-	SynNum = 0, % tak, to będzie zmienione
+	SynNum = 0, % tego wymaga sterownik stargazera
  	MsgBase = #drivermsg{type = 'DATA', synnum = SynNum},
-	{ok, Msg} = stargazer_pb:set_extension(MsgBase, subscribeaction, #subscribeaction{action = 'SUBSCRIBE',
-	                                       																						freq = proplists:get_value(freq, Os, 100) }),
+	{ok, Msg} = stargazer_pb:set_extension(MsgBase, subscribeaction,
+	                                       #subscribeaction{action = 'SUBSCRIBE',
+	                                       									freq   = proplists:get_value(freq, Os, 100) }),
 	MsgBinary = stargazer_pb:encode_drivermsg(Msg),
 	{DefDevT,DefDevI} = ?STARGAZER_TI,
 	DevT = proplists:get_value(device_type, Os, DefDevT),
 	DevI = proplists:get_value(device_id, Os, DefDevI), 
 	Hdr = #driverhdr{devicetype = DevT, deviceid = DevI},
-	OrderedBy = proplists:get_value(pid, Os, self()), 
-	register_receiver({DevT, DevI, SynNum}, OrderedBy),
+	Key = #dispd_key{dev_t=DevT, dev_i=DevI, synnum=SynNum},
+	Val = #dispd_val{recpid = proplists:get_value(pid, Os, self())},
+	register_receiver(Key, Val),
 	send_to_amber(Hdr, MsgBinary),
 	SynNum.
-
 
 stargazer_get_position(SynNum) -> stargazer_get_position(SynNum, 5000).
 
@@ -273,14 +233,17 @@ stargazer_get_position(SynNum) -> stargazer_get_position(SynNum, 5000).
 			-> #localization{}.
 stargazer_get_position(SynNum, Timeout) ->
 	{DevT,DevI} = ?STARGAZER_TI,
-	receive #amber_client_msg{hdr = #driverhdr{devicetype=DevT, deviceid=DevI}, msg=#drivermsg{synnum = SynNum} = Msg} ->
-		DMsg = stargazer_pb:decode_extensions(Msg),
-		{ok, #localizationdata{xpos=X,ypos=Y,zpos=Z,angle=A,markerid=M,timestamp=_T}}
-			= stargazer_pb:get_extension(DMsg, localizationdata),
-		#localization{xpos=X, ypos=Y, zpos=Z, angle=A, markerid=M}
+	receive #amber_client_msg{hdr = #driverhdr{devicetype=DevT, deviceid=DevI},
+														msg = #drivermsg{synnum=SynNum} = Msg} ->
+		stargazer_drivermsg_to_location(Msg)
 	after Timeout -> error(stargazer_get_position_timeout)
 	end.
 
-%% TODO
-% -spec stargazer_drivermsg_to_location(#drivermsg{})
-% 			-> #location{}.
+
+-spec stargazer_drivermsg_to_location(#drivermsg{})
+			-> #localization{}.
+stargazer_drivermsg_to_location(Msg) ->
+	DMsg = stargazer_pb:decode_extensions(Msg),
+	{ok, #localizationdata{xpos=X,ypos=Y,zpos=Z,angle=A,markerid=M,timestamp=_T}}
+							= stargazer_pb:get_extension(DMsg, localizationdata),
+	#localization{xpos=X, ypos=Y, zpos=Z, angle=A, markerid=M}.
